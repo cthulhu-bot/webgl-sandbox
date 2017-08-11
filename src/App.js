aaca6718e197c85f44d9e40aea3e978de8566f36
Josh Stupplebeen <jpstup@gmail.com>
2017-08-11 09:07:32 -0600

    moved glsl - gl shader language code to html

--- src/App.js
+++ src/App.js
@@ -48,32 +48,22 @@ class App extends Component {
 
   draw(gl) {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-    gl.drawArrays(gl.POINTS, 0, 3)
+
+    // just draw points
+    //    gl.drawArrays(gl.POINTS, 0, 3)
+
+    // just draw lines
+    // gl.drawArrays(gl.LINE_STRIP, 0, 3)
+
+    // draw an empty closed triangle
+    //    gl.drawArrays(gl.LINE_LOOP, 0, 3)
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3)
   }
 
   createShaders(gl, shaderProgram) {
-    let vs = ''
-    vs += 'attribute vec4 coords;'
-    vs += 'attribute float pointSize;'
-    vs += 'void main(void) {'
-    vs += '  gl_Position = coords;'
-    vs += '  gl_PointSize = pointSize;'
-    vs += '}'
-
-    let vertexShader = gl.createShader(gl.VERTEX_SHADER)
-    gl.shaderSource(vertexShader, vs)
-    gl.compileShader(vertexShader)
-
-    var fs = ''
-    fs += 'precision mediump float;'
-    fs += 'uniform vec4 color;'
-    fs += 'void main(void) {'
-    fs += '  gl_FragColor = color;'
-    fs += '}'
-
-    let fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
-    gl.shaderSource(fragmentShader, fs)
-    gl.compileShader(fragmentShader)
+    let vertexShader = this.getShader(gl, "shader-vs")
+    let fragmentShader = this.getShader(gl, "shader-fs")
 
     shaderProgram = gl.createProgram()
     gl.attachShader(shaderProgram, vertexShader)
@@ -85,6 +75,7 @@ class App extends Component {
   }
 
   createVertices(gl, shaderProgram, vertices) {
+    // UPDATE POINT POSITION HERE
     vertices = [
       -0.9, -0.9, 0.0,
       0.9, -0.9, 0.0,
@@ -97,7 +88,7 @@ class App extends Component {
 
     // these attribLocation vars are defined as shaderAttributes above
     let coords = gl.getAttribLocation(shaderProgram, 'coords')
-//    gl.vertexAttrib3f(coords, 0.5, 0, 0)
+    //    gl.vertexAttrib3f(coords, 0.5, 0, 0)
     gl.vertexAttribPointer(coords, 3, gl.FLOAT, false, 0, 0)
     gl.enableVertexAttribArray(coords)
     gl.bindBuffer(gl.ARRAY_BUFFER, null)
@@ -109,6 +100,51 @@ class App extends Component {
     // UPDATE COLOR HERE
     let color = gl.getUniformLocation(shaderProgram, 'color')
     gl.uniform4f(color, 0, 0, 0, 1)
+  }
+
+
+  /*
+   * https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Adding_2D_content_to_a_WebGL_context
+   */
+  getShader(gl, id) {
+    var shaderScript, theSource, currentChild, shader;
+
+    shaderScript = document.getElementById(id);
+
+    if (!shaderScript) {
+      return null;
+    }
+
+    theSource = "";
+    currentChild = shaderScript.firstChild;
+
+    while (currentChild) {
+      if (currentChild.nodeType == currentChild.TEXT_NODE) {
+        theSource += currentChild.textContent;
+      }
+
+      currentChild = currentChild.nextSibling;
+    }
+    if (shaderScript.type == "x-shader/x-fragment") {
+      shader = gl.createShader(gl.FRAGMENT_SHADER);
+    } else if (shaderScript.type == "x-shader/x-vertex") {
+      shader = gl.createShader(gl.VERTEX_SHADER);
+    } else {
+      // Unknown shader type
+      return null;
+    }
+    gl.shaderSource(shader, theSource);
+
+    // Compile the shader program
+    gl.compileShader(shader);
+
+    // See if it compiled successfully
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      alert("An error occurred compiling the shaders: " + gl.getShaderInfoLog(shader));
+      return null;
+    }
+
+    return shader;
   }
 }
 
